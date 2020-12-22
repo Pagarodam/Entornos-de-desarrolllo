57f4b13d4ae65c9883fa4db5228afc669203ef21
Patrick Galán Rodriguez <pagarodam@gmail.com>
2020-12-22 18:37:07 +0100

    Empezando a meter en carpetas

---
+++ Acepta el reto/RetoAburrimiento.java
@@ -0,0 +1,24 @@
+/* 2.2.3.1. Acepta el reto Aburrimiento en las sobremesas.
+La entrada estará compuesta de una sucesión de números positivos 
+menores que 20.000. El último, que no deberá procesarse, será un 0.
+Por cada número de la entrada el programa deberá escribir cuántas 
+cerillas son necesarias para formar un triángulo con ese número de 
+cerillas en cada lado siguiendo el esquema de la figura. */ 
+
+import java.util.Scanner;
+
+public class RetoAburrimiento
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int cerillas = sc.nextInt();
+        
+        if (cerillas <= 20000 || cerillas >= 0)
+        {
+            
+        }
+            
+    }
+}
